43f6d22d507bdad4178cd3bad75998e9fc0d0fea
Lucas <hairmare@rabe.ch>
2022-12-22 22:19:23 +0100

    fix: make timezone aware

--- tests/test_get.py
+++ tests/test_get.py
@@ -1,6 +1,6 @@
 """Tests for high-level get API."""
 
-from datetime import datetime
+from datetime import datetime, timezone
 
 from freezegun import freeze_time
 
@@ -16,7 +16,9 @@ def test_get(klangbecken_mock, archiv_mock):  # pylint: disable=unused-argument
     assert crid.show == "test"
 
     # past show
-    with freeze_time("1993-03-02"):
-        crid = cridlib.get(timestamp=datetime(1993, 3, 1, 13, 12, 00))
+    with freeze_time("1993-03-02 00:00:00 UTC"):
+        crid = cridlib.get(
+            timestamp=datetime(1993, 3, 1, 13, 12, 00, tzinfo=timezone.utc)
+        )
     assert crid.version == "v1"
     assert crid.show == "test"
